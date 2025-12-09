a59f221e245ab75364486e615d2b708a91a4778b
Prajwal0902 <139934463+Prajwal0902@users.noreply.github.com>
2025-12-09 11:56:09 +0000

    Delete src directory

--- src/main/java/com/swe7303/devops/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swe7303.devops.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.swe7303.devops.model.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-
-}
--- src/main/resources/static/js/contact.js
+++
@@ -1,6 +0,0 @@
-// Form submission handler
-document.querySelector('.contact-form form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for contacting us! We will get back to you soon.');
-    this.reset();
-});
